a44a94982ea78632025e6a20858663b2b5809582
Mirae <miraeaeaeaeae@gmail.com>
2022-03-07 15:32:04 +0900

    .

--- SQL/login_info_table.sql
+++ SQL/login_info_table.sql
@@ -3,4 +3,8 @@ create table login_info (
     login_id varchar2(20)
 );
 
+create sequence logininfo_seq
+start with 1
+increment by 1;
+
 insert into login_info values(logininfo_seq.nextval, null);
