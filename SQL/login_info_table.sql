33c35a1734a4bb3e479049856b66cfdfba9657af
Mirae <miraeaeaeaeae@gmail.com>
2022-03-08 11:26:44 +0900

    login_info_table_updated

--- SQL/login_info_table.sql
+++ SQL/login_info_table.sql
@@ -1,10 +1,11 @@
 create table login_info (
     login_no number primary key,
     login_id varchar2(20)
+    login_date varchar2(20) not null,
 );
 
 create sequence logininfo_seq
 start with 1
 increment by 1;
 
-insert into login_info values(logininfo_seq.nextval, null);
+insert into login_info values(logininfo_seq.nextval, null, sysdate);
